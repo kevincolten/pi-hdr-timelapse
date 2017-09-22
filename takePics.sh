7198ed012c04e6c7dfc237907a4a69a4850d1992
Kevin Colten <kevincolten@gmail.com>
2017-09-22 17:27:22 -0500

    put in hostname folder

---
+++ takePics.sh
@@ -0,0 +1,3 @@
+sshpass -p hello123 ssh -oStrictHostKeyChecking=no pi@picam1.local 'python ~/pi-hdr-timelapse/runhdrpi.py &'
+sshpass -p hello123 ssh -oStrictHostKeyChecking=no pi@picam2.local 'python ~/pi-hdr-timelapse/runhdrpi.py &'
+sshpass -p hello123 ssh -oStrictHostKeyChecking=no pi@picam3.local 'python ~/pi-hdr-timelapse/runhdrpi.py &'

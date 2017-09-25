c87250a79c42e138392b896aeb754a28ca9473c5
Kevin Colten <kevincolten@gmail.com>
2017-09-25 00:31:33 -0500

    json

--- runhdrpi.py
+++ runhdrpi.py
@@ -70,6 +70,6 @@ if __name__=="__main__":
     call(["avconv", "-r", "10", "-i", foldername + "/hdr/" + basename + "_%04d.jpg", "-vcodec", "libx264", "-crf",  "20", "-g", "15", foldername + '/mp4/' + timelapsename])
     f.write('Wrote video\n.')
     f.write('Current Time: ' + datetime.now().isoformat())
-    cmd = 'sshpass -p hello123 scp -r -o StrictHostKeyChecking=no ~/' + foldername + ' pi@picam1.local:~'
+    cmd = 'sshpass -p hello123 scp -r -o StrictHostKeyChecking=no ' + foldername + ' pi@picam1.local:~'
     print(cmd)
     call([cmd])
